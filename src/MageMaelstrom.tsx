47311f8c2c5edb9bd6bbd544a47f358f99bf8df2
Nicholas <nick.macdonald@torqit.ca>
2021-11-26 17:10:47 -0400

    -Styled the page and the arena a bit -Removed a bunch of default react stuff

--- src/MageMaelstrom.tsx
+++ src/MageMaelstrom.tsx
@@ -1,8 +1,13 @@
 import React from "react";
 import { Arena } from "./Arena";
+import styles from "./MageMaelstrom.module.css";
 
 export interface MageMaelstromProps {}
 
 export const MageMaelstrom: React.FC<MageMaelstromProps> = ({}) => {
-  return <Arena width={20} height={20} />;
+  return (
+    <div id={styles.mageMaelstrom}>
+      <Arena width={20} height={20} />
+    </div>
+  );
 };
