ea221f17efda986d1203becefeecc6fd6c68b94d
Nicholas <nick.macdonald@torqit.ca>
2021-12-23 13:26:10 -0400

    -CombatantIcon is now memoized and has its tooltip back

--- src/MageMaelstrom/HUD/CombatantIcon/CombatantIcon.tsx
+++ src/MageMaelstrom/HUD/CombatantIcon/CombatantIcon.tsx
@@ -12,28 +12,26 @@ export interface CombatantIconProps {
   size?: number;
 }
 
-export const CombatantIcon: React.FC<CombatantIconProps> = ({
-  name,
-  color,
-  icon,
-  horizontalFlip,
-  size,
-}) => {
-  return (
-    <div style={size ? { width: size, height: size } : {}}>
-      <div className={styles.iconWrapper} style={{ borderColor: color }}>
-        <Stack fill>
-          <img
-            className={classNames(styles.icon, {
-              [styles.flip]: horizontalFlip,
-            })}
-            src={icon}
-            alt={name}
-            width={size}
-            height={size}
-          ></img>
-        </Stack>
+export const CombatantIcon = React.memo<CombatantIconProps>(
+  ({ name, color, icon, horizontalFlip, size }) => {
+    return (
+      <div style={size ? { width: size, height: size } : {}}>
+        <Tooltip content={name}>
+          <div className={styles.iconWrapper} style={{ borderColor: color }}>
+            <Stack fill>
+              <img
+                className={classNames(styles.icon, {
+                  [styles.flip]: horizontalFlip,
+                })}
+                src={icon}
+                alt={name}
+                width={size}
+                height={size}
+              ></img>
+            </Stack>
+          </div>
+        </Tooltip>
       </div>
-    </div>
-  );
-};
+    );
+  }
+);
