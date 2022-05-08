178caee73a0535d77f537f3a99208428df06f1d1
Nicholas <nmacdonald@compassaq.com>
2022-05-08 17:37:35 -0300

    Game Day Prep

--- src/MageMaelstrom/HUD/Battle/Battle.tsx
+++ src/MageMaelstrom/HUD/Battle/Battle.tsx
@@ -8,7 +8,6 @@ import { BattleLogs } from "../BattleLogs";
 import { Controls } from "./Controls";
 import { Help } from "../Help";
 import { useTeamSelection } from "../../Logic/TeamSelectionProvider";
-import { useSimulation } from "../../Logic/SimulationProvider";
 import { SimulationModal } from "./SimulationModal";
 
 export interface BattleProps {}
@@ -16,7 +15,6 @@ export interface BattleProps {}
 export const Battle: React.FC<BattleProps> = ({}) => {
   const { resetGame, clearGame } = useTeamSelection();
   const { leftTeam, rightTeam } = useGameManager();
-  const { simulateManyGames } = useSimulation();
 
   const [seeLeft, setSeeLeft] = useState(true);
   const [seeRight, setSeeRight] = useState(true);
@@ -54,9 +52,6 @@ export const Battle: React.FC<BattleProps> = ({}) => {
         <Stack.Item>
           <Stack alignment="end">
             <Stack gap={20}>
-              <NiceButton onClick={simulateManyGames}>
-                Simulate 200 Games
-              </NiceButton>
               <NiceButton onClick={resetGame}>Restart Game</NiceButton>
               <NiceButton onClick={clearGame}>New Game</NiceButton>
             </Stack>
